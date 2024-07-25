ad953005323c3428684d5f11a82e7b4988e1f480
raysgithub17 <smdrayhan2002@gmail.com>
2024-07-25 15:11:18 +0530

    v2-2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,7 @@ export const wrapRootElement = ({ element }) => (
   <>
     <Helmet>
       <meta name="react-gatsby-webiste" content="superops website using react-gatsby" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-HP4WKPMZYR"></script>
     </Helmet>
     {element}
   </>
